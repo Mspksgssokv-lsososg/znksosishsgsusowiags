9cf9193195963a53928ca799e114ce7fe9429d11
chanujki <uca_copyright_zoon@yahoo.com>
2026-03-22 01:29:30 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,4 @@ const bot = new TelegramBot(config.token, { polling: true });
 banner();
 loader(bot);
 
-console.log('🤖 ====================================================
-            ╦═╗╔═╗╦╔═ ╦╔╗   ╔╗ ╔═╗╔╦╗
-╠╦╝╠═╣╠╩╗ ║╠╩╗  ╠╩╗║ ║ ║ 
-╩╚═╩ ╩╩ ╩ ╩╚═╝  ╚═╝╚═╝ ╩ 
-________________________________________
-OWNNER   : Rakib Chowdhury
-Facebook   : https://www.facebook.com/SYSTEM.ERROR.KING
-Whatsapp   : wa.me/+8801771306867
-
- MSG Enjoy Rakib Bot🤙
- _______________________________________ 
-RAKIB PROJECT VERSION 4.0.0
-______________________________________..');
+console.log("🤖 Bot is running...");
