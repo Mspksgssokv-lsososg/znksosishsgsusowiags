b974cd45c4dc0f98ee2d221ce6e294a4c82afdbd
chanujki <uca_copyright_zoon@yahoo.com>
2026-03-22 01:52:55 +0600

    Update config.js

--- UCA-Config/config.js
+++ UCA-Config/config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  token: "7387717029:AAF_HQ6rOXxjT0VSyANiwqoWYREXuaO03C0",
-  prefix: "/",
-  admins: [8378935950] 
+  token: "7387717029:AAF_HQ6rOXxjT0VSyANiwqoWYREXuaO03C0", 
+  prefix: "/", 
+  admins: [8378935950], 
+
+  saveAdmin: function(userId) {
+    if (!this.admins.includes(userId)) {
+      this.admins.push(userId);
+      return true;
+    }
+    return false;
+  }
 };
