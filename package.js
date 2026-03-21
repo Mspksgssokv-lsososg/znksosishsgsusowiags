8a8565c62b807591554e9445f64a8b7c64579f8f
chanujki <uca_copyright_zoon@yahoo.com>
2026-03-22 00:27:41 +0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "rakib-bot",
+  "version": "1.0.0",
+  "description": "Telegram Bot by Rakib",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "author": "Rakib",
+  "license": "MIT",
+  "dependencies": {
+    "node-telegram-bot-api": "^0.64.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.0"
+  }
+}
