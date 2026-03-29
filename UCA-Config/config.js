448a42fe3eaf798c527eac34bd536093c07d8e80
Mspksgssokv-lsososg <itssiddik487@gmail.com>
2026-03-29 11:18:31 +0600

    Update config.js

--- UCA-Config/config.js
+++ UCA-Config/config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  token: "8641802039:AAHHAVHoVD0EhtjQyXtbQZ4sYJL9g-mjuAk", 
+  token: "8641802039:AAHHAVHoVD0EhtjQyXtbQZ4sYJL9g-mjuAk",
   prefix: "/", 
-  admins: [6734899387], 
+  admins: [6734899387],
+
+  adminName: "SK-SIDDIK-KHAN", 
 
   saveAdmin: function(userId) {
     if (!this.admins.includes(userId)) {
