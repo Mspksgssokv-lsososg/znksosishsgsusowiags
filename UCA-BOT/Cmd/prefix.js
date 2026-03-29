997e4ffb5d2ed518395c523a38b64d870dfd3cc4
Mspksgssokv-lsososg <itssiddik487@gmail.com>
2026-03-29 11:28:35 +0600

    Create prefix.js

---
+++ UCA-BOT/Cmd/prefix.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: "prefix",
+    aliases: ["pre"],
+    prefix false,
+    credits: "SK-SIDDIK-KHAN",
+    description: "Bot prefix দেখার জন্য",
+
+    run: async (bot, msg, args) => {
+        const chatId = msg.chat.id;
+        const message_id = msg.message_id;
+
+        let config = {};
+        try {
+            config = require("../../UCA.Config/config.js");
+        } catch {}
+
+        const prefix = config.prefix || "/";
+
+        return bot.sendMessage(chatId,
+            `🌐 Bot Prefix: ${prefix}`,
+            { reply_to_message_id: message_id }
+        );
+    }
+};
